d9193a27d405e2826a47ea7f1a8d9e52965f5cd7
Pepe Álvarez <pepe@pixelada.org>
2022-10-20 11:43:24 +0200

    Imports in app.component and check permisions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Push } from '@awesome-cordova-plugins/push/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform : Platform, private push: Push) {
+    this.platform.ready().then(() => {
+      
+      //Check permision for push notifications
+      this.push.hasPermission().then((res: any) => {
+        if (res.isEnabled) {
+          console.log('We have permission to send push notifications');
+        } else {
+          console.log('We do not have permission to send push notifications');
+        }
+      });
+
+    });
+  }
 }
