370cfaea8db78fe0fec0cd068b7d3be8fc4b7599
Pepe Álvarez <pepe@pixelada.org>
2022-10-20 12:22:13 +0200

    Push Methods in app-components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { Push } from '@awesome-cordova-plugins/push/ngx';
+import { Push, PushObject, PushOptions } from '@awesome-cordova-plugins/push/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -19,6 +19,44 @@ export class AppComponent {
         }
       });
 
-    });
+            // Create a channel (Android O and above). You'll need to provide the id, description and importance properties.
+            this.push.createChannel({
+              id: "testchannel1",
+              description: "My first test channel",
+              // The importance property goes from 1 = Lowest, 2 = Low, 3 = Normal, 4 = High and 5 = Highest.
+              importance: 3,
+              //badge is used to if badge appears on the app icon see https://developer.android.com/reference/android/app/NotificationChannel.html#setShowBadge(boolean).
+              //false = no badge on app icon.
+              //true = badge on app icon
+              badge: false
+            }).then(() => console.log('Channel created'));
+      
+            // Delete a channel (Android O and above)
+            this.push.deleteChannel('testchannel1').then(() => console.log('Channel deleted'));
+      
+            // Return a list of currently configured channels
+            this.push.listChannels().then((channels) => console.log('List of channels', channels))
+      
+            // to initialize push notifications
+      
+            const options: PushOptions = {
+              android: {},
+              ios: {
+              alert: 'true',
+              badge: true,
+              sound: 'false'
+              },
+            }
+      
+            const pushObject: PushObject = this.push.init(options);
+            pushObject.subscribe("my-test").then ((info) => console.log('subscribed to my-test ', info));
+      
+      
+            pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+      
+            pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+      
+            pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+      });
   }
 }
